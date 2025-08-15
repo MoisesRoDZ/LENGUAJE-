b1b741a3ee0c9a418a410eab3a77a5e59cb4018c
MoisesRoDZ <moises.rodriguez@ucic.edu.mx>
2025-08-15 16:46:29 -0600

    Add files via upload

---
+++ mascota.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Mascota
+{
+    // Atributos
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Tipo { get; set; }
+    public string Sonido { get; set; }
+
+    // Constructor
+    public Mascota(string nombre, int edad, string tipo, string sonido)
+    {
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Tipo = tipo;
+        this.Sonido = sonido;
+    }
+
+    // Métodos
+    public void EmitirSonido()
+    {
+        Console.WriteLine(Sonido);
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Edad: {Edad}");
+        Console.WriteLine($"Tipo: {Tipo}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Crear una instancia de la clase Mascota
+        Mascota miMascota = new Mascota("Firulais", 3, "Perro", "Guau!");
+
+        // Llamar a los métodos
+        miMascota.EmitirSonido();
+        miMascota.MostrarInformacion();
+    }
+}
